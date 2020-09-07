f41724e7fd1402318050a64dc83c8558516c2526
Mumuni Mohammed <43080869+Kalkulus1@users.noreply.github.com>
2020-09-07 08:35:58 +0000

    Update retrieve.php

--- retrieve.php
+++ retrieve.php
@@ -3,30 +3,7 @@ include "config.php";
 
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Listing</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" >
-</head>
-<body>
-
-
-    <div class="container">
-
-                <div class="row">
-                    <div class="col-lg-12 d-flex justify-content-center">
                         <h1>Sensor Listing</h1>
-                    </div>
-                </div>
-        
-        <div class="row">
-            <div class="col-lg-12">
    
             <?php
             if(isset($_GET['listing'])){
@@ -40,16 +17,16 @@ include "config.php";
                 }
                 if   ($_GET['listing']==2){
                     $q=mysqli_query($con,"select * from `iotlab1`");
-                    echo '<table  id="myTable"  class="table table-hover table-primary" style="margin-top:2rem;" >';
+                    echo '<table>';
 
 
                     echo '<thead>
                     <tr>
                         <th>DataID</th>
                         <th>Sensor Name</th>
-                        <th scope="col">Sensor Type</th>
-                        <th scope="col">Current Reading</th>
-                        <th scope="col">Sensor Location</th>
+                        <th>Sensor Type</th>
+                        <th>Current Reading</th>
+                        <th>Sensor Location</th>
                     </tr>
                     </thead>';
                     while($row = mysqli_fetch_assoc($q)) {            
@@ -69,15 +46,3 @@ include "config.php";
         </div>
     </div>
     
-
-
-
-
-
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" ></script>
-<script src="script.js"></script>
-</body>
-</html>
